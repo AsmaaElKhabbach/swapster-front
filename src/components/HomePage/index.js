18ff9debfc83a7bfca80cf3fabb8ed747d6de5b6
AichaaZine <aicha.zine23@gmail.com>
2023-03-31 16:11:03 +0200

    adding Header component in homepage

--- src/components/HomePage/index.js
+++ src/components/HomePage/index.js
@@ -1,10 +1,12 @@
 // == Import
 import Footer from '../Partials/Footer';
+import Header from '../Partials/Header';
 
 // == Component
 function HomePage() {
   return (
     <div>
+      <Header />
       <Footer />
     </div>
   );
