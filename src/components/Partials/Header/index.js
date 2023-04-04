b90e5db5e26a1cc9c63c4e7a0760751a8180ca98
FabienCosta <fabiencosta94800@gmail.com>
2023-04-04 10:42:10 +0200

    change template bootstrap for responsive

--- src/components/Partials/Header/index.js
+++ src/components/Partials/Header/index.js
@@ -3,21 +3,37 @@ import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 import './styles.scss';
 
 function NavScrollExample() {
   return (
-    <Navbar className="navbar">
+    <Navbar className="color-nav" expand="lg">
       <Container fluid>
-        <img src="" alt="logo swapster" />
-        <Navbar className="title" href="#">Swapster</Navbar>
         <Navbar.Toggle aria-controls="navbarScroll" />
         <Navbar.Collapse id="navbarScroll">
           <Nav
             className="me-auto my-2 my-lg-0"
-            style={{ maxHeight: '100px' }}
+            style={{ maxHeight: '200px' }}
             navbarScroll
-          />
+          >
+            <Nav.Link href="#action1">Home</Nav.Link>
+            <Nav.Link href="#action2">Link</Nav.Link>
+            <NavDropdown title="Link" id="navbarScrollingDropdown">
+              <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action4">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action5">
+                Something else here
+              </NavDropdown.Item>
+            </NavDropdown>
+            <Nav.Link href="#" disabled>
+              Link
+            </Nav.Link>
+            <Navbar.Brand className="title" href="#">Swapster</Navbar.Brand>
+          </Nav>
           <Form className="d-flex">
             <Form.Control
               type="search"
@@ -25,7 +41,7 @@ function NavScrollExample() {
               className="me-2"
               aria-label="Search"
             />
-            <Button className="button">Search</Button>
+            <Button variant="danger">Search</Button>
           </Form>
         </Navbar.Collapse>
       </Container>
