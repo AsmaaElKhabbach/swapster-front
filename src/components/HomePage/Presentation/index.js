98f16470ed5f8ea546afca534070731af5faf105
FabienCosta <fabiencosta94800@gmail.com>
2023-04-04 11:58:12 +0200

    add presentation component

---
+++ src/components/HomePage/Presentation/index.js
@@ -0,0 +1,17 @@
+import './styles.scss';
+
+function Presentation() {
+  return (
+    <div className="presentation">
+      <h1>Présentation du site</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, neque dolor! Quisquam, autem eum itaque voluptates repudiandae dolore vel quaerat nulla eaque vitae debitis esse placeat odit quia dolorem. Mollitia!
+        In repellat sunt nam! Repudiandae, inventore temporibus ducimus vero et tenetur repellat deserunt magni necessitatibus aliquid cupiditate ipsum corporis minima dolores odit illo dolor itaque assumenda ut delectus. Voluptas, quibusdam?
+        Quae beatae voluptatum natus, rerum placeat soluta maiores praesentium. Neque quisquam ipsum doloribus officia nobis voluptas cupiditate nostrum aut repellendus, voluptate aliquam odio? Accusamus quia ab corporis vero sit repellat.
+        Temporibus facere reprehenderit mollitia, eligendi assumenda alias pariatur possimus. Soluta velit consequuntur porro eos quam eum fugiat saepe maiores incidunt illum repellendus, laboriosam sunt veniam obcaecati. Impedit porro architecto molestiae.
+      </p>
+    </div>
+  );
+}
+
+export default Presentation;
--- src/components/HomePage/index.js
+++ src/components/HomePage/index.js
@@ -1,10 +1,12 @@
 import NavScrollExample from '../Partials/Header/index';
-import CarouselFade from './carrousel/index';
+import CarouselFade from './Carrousel/index';
+import Presentation from './Presentation/index';
 
 function HomePage() {
   return (
     <div>
       <NavScrollExample />
+      <Presentation />
       <CarouselFade />
     </div>
   );
