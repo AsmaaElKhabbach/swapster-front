dddaa9b7dba5f9d253513a9194d42a7406613833
FabienCosta <fabiencosta94800@gmail.com>
2023-04-05 09:07:06 +0200

    commit to merge on searchbranch

--- src/index.js
+++ src/index.js
@@ -1,33 +1,27 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-import { BrowserRouter } from 'react-router-dom';
-
 // == Import : npm
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import App from 'src/components/App';
 import rootReducer from './reducers';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 // == Import : local
 // Composants
-import App from 'src/components/App';
 
 // == Render
 // 1. Élément React racine (celui qui contient l'ensemble de l'app)
 //    => crée une structure d'objets imbriqués (DOM virtuel)
 const rootReactElement = (
   <Provider store={store}>
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </Provider>
 );
 
 // 2. La cible du DOM (là où la structure doit prendre vie dans le DOM)
 const root = createRoot(document.getElementById('root'));
-
 
 // 3. Déclenchement du rendu de React (virtuel) => DOM (page web)
 root.render(rootReactElement);
