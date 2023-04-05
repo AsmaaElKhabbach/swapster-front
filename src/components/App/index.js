3908d4cf61015686829c59076ce0df60c751c52c
FabienCosta <fabiencosta94800@gmail.com>
2023-04-05 08:56:12 +0200

    commit dev to merge

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,9 +1,14 @@
 // == Import
 import { Route, Routes } from 'react-router-dom';
 import HomePage from '../HomePage';
+<<<<<<< HEAD
 import SignUp from '../SignUp';
 
 import './styles.scss';
+=======
+import './styles.scss';
+import { Routes, Route} from 'react-router-dom'
+>>>>>>> c5eca925526a51a1c5cf20bf7396b8860c0e6533
 
 // == Component
 function App() {
--- src/index.js
+++ src/index.js
@@ -1,24 +1,36 @@
 // == Import : npm
-import 'bootstrap/dist/css/bootstrap.min.css';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+<<<<<<< HEAD
 // import rootReducer from './store/reducers';
 import 'bootstrap/dist/css/bootstrap.min.css';
+=======
+>>>>>>> c5eca925526a51a1c5cf20bf7396b8860c0e6533
 
 // == Import : local
 // Composants
 import App from 'src/components/App';
-
+import store from './store';
+import 'bootstrap/dist/css/bootstrap.min.css';
 // == Render
 // 1. Élément React racine (celui qui contient l'ensemble de l'app)
 //    => crée une structure d'objets imbriqués (DOM virtuel)
 const rootReactElement = (
+<<<<<<< HEAD
   // <Provider store={store}>
+=======
+>>>>>>> c5eca925526a51a1c5cf20bf7396b8860c0e6533
   <BrowserRouter>
-    <App />
+     <Provider store={store}>
+        <App />
+    </Provider>
   </BrowserRouter>
+<<<<<<< HEAD
   // </Provider>
+=======
+ 
+>>>>>>> c5eca925526a51a1c5cf20bf7396b8860c0e6533
 );
 
 // 2. La cible du DOM (là où la structure doit prendre vie dans le DOM)
