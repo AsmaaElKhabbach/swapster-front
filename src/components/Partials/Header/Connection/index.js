2fb8c0fb3ba216decb30a8a9d9d9229ebe0d02a1
AichaaZine <aicha.zine23@gmail.com>
2023-04-06 11:33:10 +0200

    adding logout function + testing

--- src/components/Partials/Header/Connection/index.js
+++ src/components/Partials/Header/Connection/index.js
@@ -1,13 +1,21 @@
 // == Import
 import Nav from 'react-bootstrap/Nav';
 import './styles.scss'
+import { useSelector } from 'react-redux';
 // == Commponent
 function Connection() {
+  const username = useSelector((state) => state.settings.user.data.name);
+  const isLoggedIn = useSelector((state) => state.settings.isLoggedIn);
+
+
   return (
     <div className='connection'>
-      <Nav.Link href="/signup">S'inscrire</Nav.Link>
-      <Nav.Link href="/login">Se connecter</Nav.Link>
-    
+    {isLoggedIn && (
+      <p>Bienvenue {username}</p>
+    )}
+    {!isLoggedIn && (
+      <><Nav.Link href="/signup">S'inscrire</Nav.Link><Nav.Link href="/login">Se connecter</Nav.Link></>
+    )}
     </div>
   )
 }
--- src/store/reducers/settings.js
+++ src/store/reducers/settings.js
@@ -6,7 +6,10 @@ user: {
   data: {}, // contient les données de l'utilisateur
   isLoading: false, // va valoir true qd les donénes sont entrain d'etre chargées
   error: null, // va valoir une erreur lorsqu'il y a une erreur 
-}
+  logged: true,
+},
+
+isLoggedIn: false,
 
 };
 
@@ -14,11 +17,18 @@ user: {
 export const saveUser = createAction('settings/saveUser');
 export const setIsLoading = createAction('settings/setIsLoading');
 export const setError = createAction('settings/setError');
+export const logout = createAction('settings/logout');
 // == Reducer
 const settingsReducer = createReducer(initialState, (builder) => {
   builder 
   .addCase(saveUser, (state, { payload }) => {
     state.user.data = payload;
+    const {logged} = payload;
+    state.isLoggedIn = logged;
+  })
+
+  .addCase(logout, (state) => {
+    state.isLoggedIn = false;
   });
 
 });
