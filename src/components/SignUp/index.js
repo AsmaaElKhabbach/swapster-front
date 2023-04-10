deae0a92c99021e96c121c7434cb64e628d59494
AichaaZine <aicha.zine23@gmail.com>
2023-04-10 17:08:58 +0200

    controlled inputs and api call for signUp component

--- src/components/SignUp/index.js
+++ src/components/SignUp/index.js
@@ -1,3 +1,4 @@
+// == Import 
 import React from 'react';
 import {
   MDBBtn,
@@ -5,13 +6,43 @@ import {
   MDBCard,
   MDBCardBody,
   MDBInput,
-  MDBCheckbox,
 }
-  from 'mdb-react-ui-kit';
+from 'mdb-react-ui-kit';
 import Header from '../Partials/Header';
 import Footer from '../Partials/Footer';
+import { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import {signUp} from '../../api/auth';
+import { setError } from '../../store/reducers/settings';
 
+// == Component 
 function SignUp() {
+  const dispatch = useDispatch();
+
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    city: "",
+    password: "",
+    confirmPassword: ""
+  });
+
+  const { isLoading, error } = useSelector(state => state.settings.user);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (formData.password !== formData.confirmPassword) {
+      dispatch(setError("Passwords don't match."));
+      return;
+    }
+    dispatch(signUp(formData));
+  }
+
+  const handleInputChange = (event) => {
+    setFormData({ ...formData, [event.target.name]: event.target.value });
+  }
+
+  
   return (
     <>
       <Header />
@@ -23,18 +54,69 @@ function SignUp() {
         }}
       >
         <div className="mask gradient-custom-3" />
-        <MDBCard className="m-5" style={{ maxWidth: '600px' }}>
+        <MDBCard style={{ maxWidth: '650px', marginTop: 110, marginBottom: 50, }}>
           <MDBCardBody className="px-5">
-            <h2 className="text-uppercase text-center mb-5">Create an account</h2>
-            <MDBInput wrapperClass="mb-4" placeholder="Your Name" size="lg" id="form1" type="text" />
-            <MDBInput wrapperClass="mb-4" placeholder="Your Email" size="lg" id="form2" type="email" />
-            <MDBInput wrapperClass="mb-4" placeholder="Your City" size="lg" id="form3" type="city" />
-            <MDBInput wrapperClass="mb-4" placeholder="Password" size="lg" id="form4" type="password" />
-            <MDBInput wrapperClass="mb-4" placeholder="Repeat your password" size="lg" id="form4" type="password" />
-            <div className="d-flex flex-row justify-content-center mb-4">
-              <MDBCheckbox name="flexCheck" id="flexCheckDefault" label="I agree all statements in Terms of service" />
-            </div>
-            <MDBBtn className="mb-4 w-100 gradient-custom-4" size="lg">Register</MDBBtn>
+            <h2 className="text-uppercase text-center mb-5">Créer un compte</h2>
+            {error && <div className="alert alert-danger">{error}</div>}
+            <form onSubmit={handleSubmit}>
+              <MDBInput 
+              value={formData.name}
+              onChange={handleInputChange} 
+              name = "name"
+              wrapperClass="mb-4" 
+              style={{width: '500px'}} 
+              placeholder="Votre nom" 
+              size="lg" id="form1" 
+              type="text" 
+              />
+              <MDBInput 
+              value={formData.email} 
+              onChange={handleInputChange} 
+              name="email"
+              wrapperClass="mb-4" 
+              style={{width: '500px'}} 
+              placeholder="Votre email" 
+              size="lg" id="form2" 
+              type="email" 
+              />
+              <MDBInput 
+              value={formData.city} 
+              onChange={handleInputChange} 
+              name="city"
+              wrapperClass="mb-4" 
+              style={{width: '500px'}} 
+              placeholder="Votre ville" 
+              size="lg" id="form3" 
+              type="text"
+              />
+              <MDBInput 
+              value={formData.password} 
+              onChange={handleInputChange} 
+              name="password"
+              wrapperClass="mb-4" 
+              style={{width: '500px'}} 
+              placeholder="Veuillez saisir un mot de passe" 
+              size="lg" id="form4" 
+              type="password"
+              />
+              <MDBInput 
+              value={formData.confirmPassword} 
+              onChange={handleInputChange} 
+              name="confirmPassword"
+              wrapperClass="mb-4" 
+              style={{width: '500px'}} 
+              placeholder="Veuillez confirmer le mot de passe" 
+              size="lg" id="form5" 
+              type="password"
+              />
+              <MDBBtn
+              type='submit' 
+              className="mb-4 w-100 gradient-custom-4" 
+              size="lg" 
+              disabled={isLoading}>
+              Envoyer
+              </MDBBtn>
+            </form>
           </MDBCardBody>
         </MDBCard>
       </MDBContainer>
@@ -43,4 +125,5 @@ function SignUp() {
   );
 }
 
+// == Export 
 export default SignUp;
--- src/store/reducers/settings.js
+++ src/store/reducers/settings.js
@@ -18,6 +18,11 @@ export const saveUser = createAction('settings/saveUser');
 export const setIsLoading = createAction('settings/setIsLoading');
 export const setError = createAction('settings/setError');
 export const logout = createAction('settings/logout');
+
+export const signUpRequest = createAction('settings/signUpRequest');
+export const signUpSuccess = createAction('settings/signUpSucess');
+export const signUpFailed = createAction('settings/signUpFailed');
+
 // == Reducer
 const settingsReducer = createReducer(initialState, (builder) => {
   builder 
@@ -28,7 +33,23 @@ const settingsReducer = createReducer(initialState, (builder) => {
 
   .addCase(logout, (state) => {
     state.isLoggedIn = false;
-  });
+  })
+
+  .addCase(signUpRequest, (state) => {
+    state.user.isLoading = true;
+    state.user.error = null;
+  })
+
+  .addCase(signUpSuccess, (state, { payload }) => {
+    state.user.isLoading = false;
+    state.user.data = payload;
+    statusbar.isLoggedIn = true;
+  })
+
+  .addCase(signUpFailed, (state, { payload }) => {
+    state.user.isLoading = false;
+    state.user.error = payload;
+  })
 
 });
 
