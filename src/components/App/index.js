bd15025c578f3829385478d2f850f2f71811f9c4
AichaaZine <aicha.zine23@gmail.com>
2023-04-14 09:43:10 +0200

    merge test in dev branch

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -14,6 +14,8 @@ import Error from '../Error';
 
 // == Component
 function App() {
+    // grace au hook react-redux on récupère la donnée qui correspond a la valeur de la barre de recherche
+    const search = useSelector((state) => state.books.userSearch);
   return (
     <div className="app">
       {/* Mise en place du router  */}
